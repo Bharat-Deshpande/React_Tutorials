e7b918d98cc56631bf97dfbbeaec71be4ab5ca28
Bharat Deshpande <mohitssu001@gmail.com>
2024-12-08 19:20:37 +0530

    hooks

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter] = useState(15)
+  
+
+  // let counter = 15
+
+  const addValue = () =>{
+  // console.log("clicked: " + counter);  
+  //counter = counter+1
+  if(counter<20){
+    setCounter(counter+1)
+  }else{
+    alert("Limit exceeded")
+  }
+
+  }
+
+  const removeValue = () =>{
+    if(counter>0){
+      setCounter(counter-1)
+    }else{
+      alert("Limit exceeded")
+    }
+  }
+
+  return (
+    <>
+      <h1>Chai aur code</h1>
+      <h2>Counter value: {counter}</h2>
+      <button
+      onClick={addValue}>Add value</button>
+      <br />
+      <button
+      onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
